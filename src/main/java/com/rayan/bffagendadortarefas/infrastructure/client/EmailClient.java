0dad369e88b155a902648e0861a92019cce732c7
Rayan <rayan.almeida0@outlook.com>
2026-08-22 17:59:51 -0300

    Adicionado client de email

---
+++ src/main/java/com/rayan/bffagendadortarefas/infrastructure/client/EmailClient.java
@@ -0,0 +1,11 @@
+package com.rayan.bffagendadortarefas.infrastructure.client;
+
+import com.rayan.bffagendadortarefas.business.dto.out.TarefasDTOResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "notificacao", url = "${notificacao.url}")
+public interface EmailClient {
+
+    void enviarEmail(@RequestBody TarefasDTOResponse dto);
+}
